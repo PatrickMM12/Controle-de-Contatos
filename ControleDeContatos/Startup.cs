7dab43ff6214d60a00b905df4999f848d655ffdc
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-23 16:28:08 -0300

    Criação do Banco de dados através do Entity Framework Core

---
+++ ControleDeContatos/Startup.cs
@@ -0,0 +1,49 @@
+using ControleDeContatos.Models.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // Add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<BancoContext>(
+                    options => options.UseSqlServer(Configuration.GetConnectionString("DataBase")));
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
+
