d63361fcfdfd73ee0c42fd352b0f6c65810bcdb2
PatrickMM12 <patrickmacedo05@hotmail.com>
2022-04-24 19:24:52 -0300

    Alerta de Sucesso e de Erro para cadastro de contato

--- ControleDeContatos/Controllers/ContatoController.cs
+++ ControleDeContatos/Controllers/ContatoController.cs
@@ -5,68 +5,76 @@ using Microsoft.AspNetCore.Mvc;
 namespace ControleDeContatos.Controllers
 {
 
-    public class ContatoController : Controller
-    {
-        private readonly IContatoRepositorio contatoRepositorio;
+	public class ContatoController : Controller
+	{
+		private readonly IContatoRepositorio contatoRepositorio;
 
-        public ContatoController(IContatoRepositorio contatoRepositorio)
-        {
-            this.contatoRepositorio = contatoRepositorio;
-        }
+		public ContatoController(IContatoRepositorio contatoRepositorio)
+		{
+			this.contatoRepositorio = contatoRepositorio;
+		}
 
-        public IActionResult Index()
-        {
-            List<ContatoModel> contatos = contatoRepositorio.BuscarTodos();
-            return View(contatos);
-        }
+		public IActionResult Index()
+		{
+			List<ContatoModel> contatos = contatoRepositorio.BuscarTodos();
+			return View(contatos);
+		}
 
-        public IActionResult Criar()
-        {
-            return View();
-        }
+		public IActionResult Criar()
+		{
+			return View();
+		}
 
-        public IActionResult Editar(int id)
-        {
-            ContatoModel contato = contatoRepositorio.ListarPorId(id);
-            return View(contato);
-        }
+		public IActionResult Editar(int id)
+		{
+			ContatoModel contato = contatoRepositorio.ListarPorId(id);
+			return View(contato);
+		}
 
-        public IActionResult ApagarConfirmacao(int id)
-        {
-            ContatoModel contato = contatoRepositorio.ListarPorId(id);
-            return View(contato);
-        }
+		public IActionResult ApagarConfirmacao(int id)
+		{
+			ContatoModel contato = contatoRepositorio.ListarPorId(id);
+			return View(contato);
+		}
 
-        public IActionResult Apagar(int id)
-        {
-            contatoRepositorio.Apagar(id);
-            return RedirectToAction("Index");
-        }
+		public IActionResult Apagar(int id)
+		{
+			contatoRepositorio.Apagar(id);
+			return RedirectToAction("Index");
+		}
 
-        [HttpPost]
-        public IActionResult Criar(ContatoModel contato)
-        {
+		[HttpPost]
+		public IActionResult Criar(ContatoModel contato)
+		{
+			try
+			{
+				if (ModelState.IsValid)
+				{
+					contatoRepositorio.Adicionar(contato);
+					TempData["MensagemSucesso"] = "Contato cadastrado com sucesso";
+					return RedirectToAction("Index");
+				}
 
-            if (ModelState.IsValid)
-            {
-                contatoRepositorio.Adicionar(contato);
-                return RedirectToAction("Index");
-            }
+				return View(contato);
+			}
+			catch (System.Exception erro)
+			{
+				TempData["MensagemErro"] = $"Ops, não conseguimos cadastrar seu contato, tente novamente. Detalhe do erro: {erro.Message}";
+				return RedirectToAction("Index");
+			}
+		}
 
-            return View(contato);
-        }
+		[HttpPost]
+		public IActionResult Editar(ContatoModel contato)
+		{
 
-        [HttpPost]
-        public IActionResult Editar(ContatoModel contato)
-        {
+			if (ModelState.IsValid)
+			{
+				contatoRepositorio.Atualizar(contato);
+				return RedirectToAction("Index");
+			}
 
-            if (ModelState.IsValid)
-            {
-                contatoRepositorio.Atualizar(contato);
-                return RedirectToAction("Index");
-            }
-
-            return View(contato);
-        }
-    }
+			return View(contato);
+		}
+	}
 }
